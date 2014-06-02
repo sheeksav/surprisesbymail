6fc3e12f78b90067bac8323e0fc7558b80602cf0
sheeksav <andy.sheeks@gmail.com>
2014-06-02 19:01:39 -0400

    Added Bootstrap and basic template setup.

--- surprisesbymail/urls.py
+++ surprisesbymail/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from store.views import HomeView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'surprisesbymail.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomeView.as_view(), name='home'),
+    
 )
